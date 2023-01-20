dc318d2a3f0aa76d8a4f3180d637f5a198fdd9fa
francesco_cecco <francesco.cecconello@studenti.univr.it>
2023-01-20 16:02:29 +0100

    commit

---
+++ esame/generated/forms.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+from django import forms
+
+class answer(forms.Form):
+    ans_opt_sol = forms.CharField(max_length = 200,required=False)
+    ans_opt_val = forms.CharField(max_length = 200,required=False)
+
+class uploaded_file(forms.Form):
+    file = forms.FileField()
